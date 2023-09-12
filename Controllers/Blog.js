8bb4f8c7c510c53b345121be3ebcd6d8ac05c883
sanae ouajhain <sanae>
2023-09-12 15:10:33 +0100

    commit

--- Controllers/Blog.js
+++ Controllers/Blog.js
@@ -21,11 +21,23 @@ exports.createBlog = (req,res) => {
 
     axios.post('http://localhost:3000/blogs', newBlog)
     return res.redirect('/allblogs')
-    // res.end()
-}
-
+    // res.end() axios.post('http://localhost:3000/blogs', newBlog)
+        .then(() => {
+            // La création du blog s'est bien déroulée, rediriger ou renvoyer une réponse réussie.
+            return res.redirect('/allblogs'); // Ou renvoyez un message de réussite si nécessaire.
+        })
+        .catch((error) => {
+            console.error(error);
+            // Une erreur s'est produite lors de la création du blog.
+            return res.status(500).json({
+                error: 'Internal Server Error',
+                message: 'Une erreur interne du serveur s\'est produite lors du traitement de votre demande. Veuillez réessayer ultérieurement.'
+            });
+        });
+    }
 exports.getBlogs = async (req,res) => {
     const fetchBlog = await axios.get('http://localhost:3000/blogs')
     const blogs = await fetchBlog.data
     res.render('allBlogs',{blogs})
-    res.end()}
+    res.end()
+}
